bcca39da9b3a009c348e35b357d9fb57d58b9ebe
Garik <gar.hovsepian@gmail.com>
2024-01-12 15:02:22 +0400

    TS-1 Initiated empaty project

---
+++ Magic QR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic QR
+//
+//  Created by Garik Hovsepian on 12.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
